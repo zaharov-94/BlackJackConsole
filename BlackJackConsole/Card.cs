53cb7207de27f8ade1e10602a5cd8a959eac9440
Anuitex-43 <thetytn@gmail.com>
2018-01-17 15:23:53 +0200

    1

--- BlackJackConsole/Card.cs
+++ BlackJackConsole/Card.cs
@@ -7,8 +7,20 @@ namespace BlackJackConsole
     /// </summary>
     public class Card
     {
-        public Suite Suite { get; set; }
-        public Worth Worth { get; set; }
-        public int Value { get; set; }
+        public Suite Suite
+        {
+            get;
+            set;
+        }
+        public Worth Worth
+        {
+            get;
+            set;
+        }
+        public int Value
+        {
+            get;
+            set;
+        }
     }
 }
--- BlackJackConsole/Player.cs
+++ BlackJackConsole/Player.cs
@@ -35,6 +35,5 @@ namespace BlackJackConsole
         {
             _cards.Add(card);
         }
-
     }
 }
