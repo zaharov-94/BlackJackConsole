f72bfe77c25d58553f74df5585fbe0a0204b5f9a
Anuitex-43 <thetytn@gmail.com>
2018-01-18 18:01:55 +0200

    some commented parts

--- BlackJackConsole/CardDesk.cs
+++ BlackJackConsole/CardDesk.cs
@@ -29,22 +29,36 @@ namespace BlackJackConsole
 
         private void FillDesk() //Убрать цикл в цикле!
         {
-            foreach (Suite suite in Enum.GetValues(typeof(Suite)))
-            {
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Two, Value = 2 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Three, Value = 3 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Four, Value = 4 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Five, Value = 5 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Six, Value = 6 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Seven, Value = 7 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Eight, Value = 8 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Nine, Value = 9 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Ten, Value = 10 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Jack, Value = 10 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Queen, Value = 10 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.King, Value = 10 });
-                _desk.Push(new Card { Suite = suite, Worth = Worth.Ace, Value = 11 });
-            }
+            Worth worth = Worth.Two;
+            Suite suite = Suite.Clubs;
+            //Первый вариант
+            //foreach (Suite suite in Enum.GetValues(typeof(Suite)))
+            //{
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Two, Value = 2 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Three, Value = 3 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Four, Value = 4 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Five, Value = 5 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Six, Value = 6 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Seven, Value = 7 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Eight, Value = 8 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Nine, Value = 9 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Ten, Value = 10 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Jack, Value = 10 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Queen, Value = 10 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.King, Value = 10 });
+            //    _desk.Push(new Card { Suite = suite, Worth = Worth.Ace, Value = 11 });
+            //}
+            //Второй вариант
+            //while ((worth <= Worth.Ace))
+            //{
+            //    _desk.Push(new Card { Suite = suite, Worth = worth, Value = 2 });
+            //    if (suite == Suite.Spades)
+            //    {
+            //        suite = Suite.Clubs;
+            //        worth++;
+            //    }
+            //    suite++;
+            //}
         }
     }
 }
